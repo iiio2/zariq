8229e4c69040dc814d69c175547b2f4148481c96
Mr. Robot <gomexxxx71@gmail.com>
2024-07-13 00:44:02 +0600

    chore: build hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Hero from './compoentts/hero'
 import Navbar from './compoentts/Navbar'
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   )
 }
---
+++ src/compoentts/hero.tsx
@@ -0,0 +1,38 @@
+const Hero = () => {
+  return (
+    <div className="container hero">
+      <div className="row">
+        <div className="col-sm-6">
+          <p>LEARN. PLAY. EXPLORE</p>
+          <h2>A safe and joyful place for your lovely children</h2>
+          <p className="hero-content">
+            Assalamu Alaikum! Welcome to Reverie School, where academic
+            excellence meets moral values. Located at Bashundhara Residential
+            Area in Dhaka, our English medium institution offers a holistic
+            education, nurturing students to thrive academically and
+            spiritually. Join us for a journey of learning and enlightenment.
+          </p>
+          <div>
+            <button className="btn btn-primary fw-bold p-1 p-3">
+              Enroll your kid
+            </button>
+          </div>
+        </div>
+        <div className="col-sm-6 hero-gallery">
+          <img
+            src="https://www.reverie.school/wp-content/uploads/2024/02/Home-3.jpg"
+            alt="hero-image"
+            className="img-fluid rounded"
+          />
+          <img
+            src="https://www.reverie.school/wp-content/uploads/2024/02/Home-2-1.jpg"
+            alt="hero-image-2"
+            className="img-fluid rounded hero-image-2"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
