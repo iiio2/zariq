75d5e672c56e438ad0408835d00a2dda1082bb4a
Mr. Robot <gomexxxx71@gmail.com>
2024-07-13 12:22:38 +0600

    styling: final section

--- src/compoentts/Final.tsx
+++ src/compoentts/Final.tsx
@@ -1,21 +1,23 @@
 const Final = () => {
   return (
     <div className="container final bg-danger">
-      <h5>Final Exam 2023-2024</h5>
+      <h5 className="text-white text-center fw-bold mt-5">
+        Final Exam 2023-2024
+      </h5>
       <div className="d-flex justify-content-center">
-        <div className="first-item">
+        <div className="first-item text-center text-white fw-bold text-white p-1">
           <div>00</div>
           <div>Days</div>
         </div>
-        <div className="second-item">
+        <div className="second-item text-center fw-bold text-white p-1">
           <div>00</div>
           <div>Hours</div>
         </div>
-        <div className="third-item">
+        <div className="third-item text-white text-center fw-bold p-1">
           <div>00</div>
           <div>Minutes</div>
         </div>
-        <div className="forth-item">
+        <div className="forth-item text-white text-center fw-bold p-1">
           <div>00</div>
           <div>Seconds</div>
         </div>
